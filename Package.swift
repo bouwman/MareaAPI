31e90a20954769b13950f20b4b34888120719c10
Tassilo Bouwman <t.bouwman@me.com>
2022-11-13 16:44:13 +0100

    initial commit

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Marea",
+    platforms: [.iOS(.v13), .macCatalyst(.v13), .macOS(.v10_15), .watchOS(.v6), .tvOS(.v13)],
+    products: [
+        .library(name: "Marea", targets: ["Marea"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/kean/Get", from: "1.0.2"),
+        .package(url: "https://github.com/CreateAPI/URLQueryEncoder", from: "0.2.0")
+    ],
+    targets: [
+        .target(name: "Marea", dependencies: [
+            .product(name: "Get", package: "Get"),
+            .product(name: "URLQueryEncoder", package: "URLQueryEncoder")
+        ], path: "Sources")
+    ]
+)
